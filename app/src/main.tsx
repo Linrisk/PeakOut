8c464a925ee2872548d6fb1f7321761d81626a57
Linrisk <45262257+Linrisk@users.noreply.github.com>
2024-01-16 09:16:18 +0100

    ajout routes

--- app/src/main.tsx
+++ app/src/main.tsx
@@ -8,25 +8,34 @@ import "./index.css";
 import SignIn from './pages/SignIn';
 import SignOut from './pages/SignOut';
 import HomePage from './pages/HomePage';
-import LogIn from './pages/LogIn';
+import HomeView from './pages/connected/HomeView';
+import Login from './pages/LogIn';
+import Profile from './pages/connected/Profile';
+import Discover from './pages/connected/Discover';
+import ChatView from './pages/connected/ChatView';
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+//import { getAuth } from "firebase/auth";
+ 
 
-//import firebase from 'firebase';
-//import { initializeApp } from "firebase/app";
-
-/*const firebaseConfig = {
-  apiKey: process.env.REACT_APP_apiKey,
-  authDomain: process.env.REACT_APP_authDomain,
-  projectId: process.env.REACT_APP_projectId,
-  storageBucket: process.env.REACT_APP_storageBucket,
-  messagingSenderId: process.env.REACT_APP_messagingSenderId,
-  appId: process.env.REACT_APP_appId,
-  measurementId: process.env.REACT_APP_measurementId
+const firebaseConfig = {
+  apiKey: "AIzaSyD_xaXDa2TdR8U3tcOIra65huNhB4VPCQE",
+  authDomain: "peak-out-2023.firebaseapp.com",
+  databaseURL: "https://peak-out-2023-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "peak-out-2023",
+  storageBucket: "peak-out-2023.appspot.com",
+  messagingSenderId: "431135230485",
+  appId: "1:431135230485:web:36e50f165b1b8b4fa49c1c",
+  measurementId: "G-431XPY2LXC"
 };
-*/
-//const app = initializeApp(firebaseConfig);
 
-
-
+try {
+  const app = initializeApp(firebaseConfig);
+  const analytics = getAnalytics(app);
+  console.log("Firebase initialized successfully");
+} catch (error) {
+  console.error("Error initializing Firebase:", error);
+}
 const router = createBrowserRouter([
   {
     path: "/",
@@ -38,11 +47,27 @@ const router = createBrowserRouter([
   },
   {
     path: "/signOut",
-    element: <SignOut />,
+    element: <SignOut/>,
+  },
+  {
+    path: "/app",
+    element: <HomeView/>,
   },
   {
     path: "/login",
-    element: <LogIn />,
+    element: <Login/>,
+  },
+  {
+    path: "/profile",
+    element: <Profile/>,
+  },
+  {
+    path: "/discover",
+    element: <Discover/>,
+  },
+  {
+    path: "/chatView",
+    element: <ChatView/>,
   }
 
 ]);
